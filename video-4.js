77be2294df1fc5aea9281ce39f2d856f45a116d6
Online Web Tutor <33731274+owthub@users.noreply.github.com>
2018-07-22 14:14:40 +0530

    Data Binding in VueJs - Video#4
    Data Binding in VueJs - Video#4

---
+++ video-4.js
@@ -0,0 +1,13 @@
+var myapp = new Vue({
+  el:"#my-app",
+  data:{
+    name:"Online Web tutor",
+    title:"This is channel link",
+    imageLink:"gallery1.png",
+    isActive:true,
+    ulink:"https://www.youtube.com/channel/UCB2flCo-gW6RhpVVXySqcMg/playlists",
+    txtName:"Sanjay Kumar",
+    txtEmail:"sanay@gmail.com",
+    htmlContent:"<p>This is my paragraph set from JS file</p>"
+  }
+});
